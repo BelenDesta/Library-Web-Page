814060b2f0691a17cf0c51b6b7d08200ad342b96
Arthur Liao <arthurliao@wireless-10-105-238-225.umd.edu>
2023-12-12 17:03:02 -0500

    trying something

--- library.js
+++ library.js
@@ -527,7 +527,7 @@ async function viewCatalog(){
 
 }
 
-app.post("/searchBook", (request, response) => {
+app.post("https://test2-t7c9.onrender.com/searchBook", (request, response) => {
     let {bookTitle, bookAuthor} = request.body;
 
     const variables = {
